d471987cb4d728394aaf860ec30bacda23af47d5
Nick Salch <nick.salch@outlook.com>
2021-12-06 09:47:42 -0800

    Update CreateRowgroupHealthView.sql
    replace pdw_table_mappings with pdw_permanent_table_mappings

--- TSQL_Queries/Indexes/CreateRowgroupHealthView.sql
+++ TSQL_Queries/Indexes/CreateRowgroupHealthView.sql
@@ -21,7 +21,7 @@ select   tb.[name]                    AS [logical_table_name]
 ,        mp.[physical_name]           AS physical_name
 FROM    sys.[schemas] sm
 JOIN    sys.[tables] tb               ON  sm.[schema_id]          = tb.[schema_id]                             
-JOIN    sys.[pdw_table_mappings] mp   ON  tb.[object_id]          = mp.[object_id]
+JOIN    sys.[pdw_permanent_table_mappings] mp   ON  tb.[object_id]          = mp.[object_id]
 JOIN    sys.[pdw_nodes_tables] nt     ON  nt.[name]               = mp.[physical_name]
 JOIN    sys.[dm_pdw_nodes_db_column_store_row_group_physical_stats] rg      ON  rg.[object_id]     = nt.[object_id]
                                                                             AND rg.[pdw_node_id]   = nt.[pdw_node_id]
